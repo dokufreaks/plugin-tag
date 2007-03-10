2cfa80fc0888ae382a5b4103cc8671023f061039
wikidesign <chi@chimeric.de>
2007-03-10 01:16:07 +0100

    29 Changed default settings: now points to 'tag' namespace and does not ping Technorati by default
    darcs-hash:20070310001607-89468-37195c4fa3e153db8ff667c4631ef4b4c03ff9bf.gz

--- conf/default.php
+++ conf/default.php
@@ -2,7 +2,7 @@
 /**
  * Options for the Tag Plugin
  */
-$conf['namespace']      = 'wiki'; // where should tag links point to? default: 'wiki'
-$conf['pingtechnorati'] = 1;      // should plugin ping Technorati?
+$conf['namespace']      = 'tag'; // where should tag links point to? default: 'wiki'
+$conf['pingtechnorati'] = 0;     // should plugin ping Technorati?
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :

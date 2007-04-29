a3c076b8a0e75f8dfc00e98ea167df80c5b92a67
lukas.zapletal <chi@chimeric.de>
2007-04-29 23:48:31 +0200

    33 Czech translation
    darcs-hash:20070429214831-d7814-537409dca8a0d7586b3153f0b16be76d569f5aac.gz

---
+++ lang/cs/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Czech language file (UTF-8 encoding)
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Lukas Zapletal <lukas.zapletal at gmail dot com>
+ */
+
+// custom language strings for the plugin
+$lang['tags']  = 'Tagy';
+$lang['topic'] = 'Téma';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/cs/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Czech language file (UTF-8 encoding)
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Lukas Zapletal <lukas.zapletal at gmail dot com>
+ */
+ 
+// for the configuration manager
+$lang['namespace']      = 'Implicitní jmenný prostor pro tagy';
+$lang['pingtechnorati'] = 'Posílat ping na Technorati';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :

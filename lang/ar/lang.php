07b7d546504d14010ad65057a9ffec4fdcd12b58
wikidesign <chi@chimeric.de>
2007-07-21 11:55:58 +0200

    34 Arabic translation by Muhammad Bashir Al-Noimi
    darcs-hash:20070721095558-89468-9188a8321db0ccb4e28dde4225db7378c40d554d.gz

---
+++ lang/ar/lang.php
@@ -0,0 +1,14 @@
+﻿<?php
+/**
+* Arabic language file
+*
+* @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+* @author    Muhammad Bashir Al-Noimi <bashir.storm@gmail.com>
+*		http://www.hali-sy.com
+*/
+
+// custom language strings for the plugin
+$lang['tags']  = 'الأقسام';
+$lang['topic'] = 'الموضوع';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/ar/settings.php
@@ -0,0 +1,14 @@
+﻿<?php
+/**
+* Arabic language file
+*
+* @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+* @author    Muhammad Bashir Al-Noimi <bashir.storm@gmail.com>
+*		http://www.hali-sy.com
+*/
+ 
+// for the configuration manager
+$lang['namespace']      = 'فضاء الأسماء الإفتراضي للأقسام';
+$lang['pingtechnorati'] = ' إعلام Technorati بكل جديد ';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :

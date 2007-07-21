f7628abff33b34d73a558d745a31adcfea425b8d
wikidesign <chi@chimeric.de>
2007-07-21 13:00:59 +0200

    35 Japanese translation by Ikuo Obataya
    darcs-hash:20070721110059-89468-dafc979c66ca16961c26b7c7afd14a46940b7742.gz

---
+++ lang/ja/lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Japanese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Ikuo Obataya <cxx05051@nifty.com>
+ */
+
+// custom language strings for the plugin
+$lang['tags']  = 'タグ';
+$lang['topic'] = 'トピック';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/ja/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Japanese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Ikuo Obataya <cxx05051@nifty.com>
+ */
+ 
+// for the configuration manager
+$lang['namespace']      = 'タグのデフォルト名前空間';
+$lang['pingtechnorati'] = 'Technoratiにpingを送信';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :

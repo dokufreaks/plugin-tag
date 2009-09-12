3c4b5e49ed93d25588fbf0342aaf71a8eb963923
Digna González Otero <digna.gonzalezotero [at] gmail [dot] com>
2009-09-12 14:40:45 +0200

    added spanish translation

---
+++ lang/es/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Spanish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Digna González Otero <digna.gonzalezotero [at] gmail [dot] com>
+ */
+// custom language strings for the plugin
+$lang['tags']  = 'Etiquetas';
+$lang['topic'] = 'Tema';
+ 
+$lang['missing_pagelistplugin'] = 'Para listas de temas debe estar instalado el Plugin Pagelist Plugin.';
+ 
+$lang['menu'] = 'Gestor de Índice de Etiquetas';
+ 
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/es/settings.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Spanish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Digna González Otero <digna.gonzalezotero [at] gmail [dot] com>
+ */
+ 
+// for the configuration manager
+$lang['namespace']      = 'Espacio de nombres por defecto para las etiquetas';
+$lang['pingtechnorati'] = 'Ping Technorati';
+ 
+$lang['sortkey']                = 'ordenar las listas de temas por';
+$lang['sortkey_o_cdate']        = 'fecha de creación';
+$lang['sortkey_o_mdate']        = 'fecha de modificación';
+$lang['sortkey_o_pagename']     = 'nombre de la página';
+$lang['sortkey_o_id']           = 'ID de la página';
+$lang['sortkey_o_title']        = 'título';
+ 
+$lang['sortorder']              = 'tipo de ordenación';
+$lang['sortorder_o_ascending']  = 'ascendente';
+$lang['sortorder_o_descending'] = 'descendente';
+ 
+$lang['pagelist_flags'] = 'Flags de formato para la lista de tags (separadas por coma, para ver los flags consultar la documentación del plugin pagelist)';
+ 
+//Setup VIM: ex: et ts=2 enc=utf-8 :

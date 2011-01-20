3423a13792423ddc731ac99e801e6ebed008195a
Matthias Schulte <post@lupo49.de>
2011-01-20 12:26:02 +0100

    zh-tw: added traditional chinese localization

---
+++ lang/zh-tw/lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Traditional Chinese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Danny Lin <danny0838@pchome.com.tw>
+ */
+
+// custom language strings for the plugin
+$lang['tags']  = '標籤';
+$lang['topic'] = '主題';
+
+$lang['missing_pagelistplugin'] = '必須安裝 Pagelist 插件才能列出主題。';
+
+$lang['menu'] = '標籤索引管理器';
+$lang['rebuildindex'] = '重建索引';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
---
+++ lang/zh-tw/settings.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Traditional Chinese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Danny Lin <danny0838@pchome.com.tw>
+ */
+ 
+// for the configuration manager
+$lang['namespace']      = '標籤預設的命名空間';
+$lang['pingtechnorati'] = '自動通知 Technorati';
+
+$lang['sortkey']                = '主題排序方式';
+$lang['sortkey_o_cdate']        = '建立日期';
+$lang['sortkey_o_mdate']        = '修改日期';
+$lang['sortkey_o_pagename']     = '頁面名稱';
+$lang['sortkey_o_id']           = '頁面全名';
+$lang['sortkey_o_title']        = '頁面標題';
+
+$lang['sortorder']              = '排序方式';
+$lang['sortorder_o_ascending']  = '升序';
+$lang['sortorder_o_descending'] = '降序';
+
+$lang['pagelist_flags'] = '標籤列表的格式化參數 (逗號分隔，可用的參數參見 pagelist 插件的說明文件)';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :

a6db1ae137da3cfe9daf15196465657f5d4d669e
Michael Hamann <michael@content-space.de>
2012-03-07 22:53:30 +0100

    Major rewrite of the count syntax, this should fix issue #48

--- syntax/count.php
+++ syntax/count.php
@@ -42,60 +42,40 @@ class syntax_plugin_tag_count extends DokuWiki_Syntax_Plugin {
     }
 
     function handle($match, $state, $pos, &$handler) {
-        
+
         $dump = trim(substr($match, 8, -2));     // get given tags
         $dump = explode('&', $dump);             // split to tags and allowed namespaces 
         $tags = $dump[0];
         $allowedNamespaces = explode(' ', $dump[1]); // split given namespaces into an array
-        
+
         if($allowedNamespaces && $allowedNamespaces[0] == '') {
             unset($allowedNamespaces[0]);    // When exists, remove leading space after the delimiter
-            $allowedNamespaces = array_values($allowedNamespaces);                
+            $allowedNamespaces = array_values($allowedNamespaces);
         }
-        
-        if (!$tags) return false;
+
+        if (empty($allowedNamespaces)) $allowedNamespaces = null;
+
+        if (!$tags) $tags = '+';
 
         if(!($my = plugin_load('helper', 'tag'))) return false;
-        $tags = $my->_parseTagList($tags);
-        
-        // get tags and their related occurences
-        // we need all tags to discover the related pages and namespaces
-        $occurences = $my->tagOccurences($tags);
 
-        // no tags given, list all tags for allowed namespaces
+        return array($my->_parseTagList($tags), $allowedNamespaces);
+    }
+
+    function render($mode, &$renderer, $data) {
+        if ($data == false) return false;
+        if(!($my = plugin_load('helper', 'tag'))) return false;
+
+        list($tags, $allowedNamespaces) = $data;
+
+        // get tags and their occurences
         if($tags[0] == '+') {
-            $pages = array();
-            $listedTag = implode(' ', array_keys($occurences)); // getTopic() needs tags seperated by spaces
-
-            foreach($allowedNamespaces as $ns) {
-                $tmppages = $my->getTopic($ns, '', $listedTag);
-                array_push($pages, $tmppages);                // holds all pages in allowed namespaces
-            }
-            
-            $tags = array();    // remove old tags
-            
-            // discover tags of the stored pages
-            foreach($pages as $entry) { 
-                foreach($entry as $page => $details) {
-                    $tmptags = p_get_metadata($details['id'], 'subject', METADATA_DONT_RENDER);
-                    foreach($tmptags as $singletag) {    // store tags on first level inside the array
-                        array_push($tags, $singletag);
-                    }
-                }
-            }
-            $tags = array_unique($tags);    // remove tag duplicates
-
+            // no tags given, list all tags for allowed namespaces
             $occurences = $my->tagOccurences($tags, $allowedNamespaces, true);
         } else {
             $occurences = $my->tagOccurences($tags, $allowedNamespaces);
         }
-        
-        return $occurences;
-    }      
 
-    function render($mode, &$renderer, $data) {
-        if(!($my = plugin_load('helper', 'tag'))) return false;
-        
         // $data -> tag as key; value as count of tag occurence
         $class = "inline"; // valid: inline, ul, pagelist
         $col = "page";
@@ -106,18 +86,14 @@ class syntax_plugin_tag_count extends DokuWiki_Syntax_Plugin {
             $renderer->doc .= '<th class="'.$col.'">tag</th>';
             $renderer->doc .= '<th class="'.$col.'">#</th>';
             $renderer->doc .= DOKU_LF.DOKU_TAB.'</tr>'.DOKU_LF;
-            
-            // skip output, if the individual occurences of the tags is zero
-            // if the amount of tags with count == 0 is equal to the overall count of tags then skip output
-            $countTags = array_keys($data, 0);
-            
-            if(sizeof($countTags) == sizeof($data)) {
+
+            if(empty($occurences)) {
                 // Skip output
                 $renderer->doc .= DOKU_TAB.'<tr>'.DOKU_LF.DOKU_TAB.DOKU_TAB;
                 $renderer->doc .= DOKU_TAB.DOKU_TAB.'<td class="'.$class.'" colspan="2">'.$this->getLang('empty_output').'</td>'.DOKU_LF;
                 $renderer->doc .= DOKU_LF.DOKU_TAB.'</tr>'.DOKU_LF;
             } else {
-                foreach($data as $tagname => $count) {
+                foreach($occurences as $tagname => $count) {
                     if($count <= 0) continue; // don't display tags with zero occurences
                     $renderer->doc .= DOKU_TAB.'<tr>'.DOKU_LF.DOKU_TAB.DOKU_TAB;
                     $renderer->doc .= DOKU_TAB.DOKU_TAB.'<td class="'.$class.'">'.$my->tagLink($tagname).'</td>'.DOKU_LF;

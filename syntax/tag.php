5b164aef1f06506f451eb73b3696669939a987fe
YoBoY <yoboy.leguesh@gmail.com>
2012-03-16 10:59:17 +0100

    $tags not declared, use of $data to replace it

--- syntax/tag.php
+++ syntax/tag.php
@@ -78,7 +78,7 @@ class syntax_plugin_tag_tag extends DokuWiki_Syntax_Plugin {
             // update tags in topic.idx
             $my->_updateTagIndex($ID, $this->tags[$ID]);
 
-            if ($renderer->capture) $renderer->doc .= DOKU_LF.strip_tags($tags).DOKU_LF;
+            if ($renderer->capture) $renderer->doc .= DOKU_LF.implode(' ', $data).DOKU_LF;
 
             // add references for the tags
             foreach ($this->tags[$ID] as $tag) {

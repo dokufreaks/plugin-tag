bb613f29b8587d99e69cc116a77b2337812cee03
haobug <qingxianhao@gmail.com>
2013-05-03 02:28:09 +0800

    Simplified Chinese translation

---
+++ lang/zh/lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Simplified Chinese language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     haobug <qingxianhao@gmail.com>
+ */
+
+// custom language strings for the plugin
+$lang['tags']  = '标签';
+$lang['topic'] = '主题';
+$lang['toolbar_icon'] = '插入标签';
+$lang['empty_output'] = '输出为空';
+
+$lang['missing_pagelistplugin'] = '必须安装 Pagelist 插件才能显示主题。';
+
+// search form of tagsearch syntax
+$lang['include'] = '包括';
+$lang['exclude'] = '排除';
+$lang['use_and'] = '使用 AND';
+$lang['no_tags'] = '没有找到标签';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :

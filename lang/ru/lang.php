ba2b05a2d2e6b2cb93af42ffdd8d384073371b51
Aleksandr Selivanov <alexgearbox@gmail.com>
2013-08-04 16:30:05 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+$lang['tags']                  = 'Теги';
+$lang['topic']                 = 'Темы';
+$lang['missing_pagelistplugin'] = 'Для отображения списка тем должен быть установлен плагин Pagelist.';

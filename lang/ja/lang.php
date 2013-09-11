71cbe7e70c2031a3cd57a0c17b95f052c4e7ab97
Hideaki SAWADA <chuno@live.jp>
2013-09-11 17:30:05 +0200

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
- * Japanese language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Ikuo Obataya <cxx05051@nifty.com>
+ * @author Hideaki SAWADA <chuno@live.jp>
  */
-
-// custom language strings for the plugin
-$lang['tags']  = 'タグ';
-$lang['topic'] = 'トピック';
-
-//Setup VIM: ex: et ts=2 enc=utf-8 :
+$lang['tags']                  = 'タグ';
+$lang['topic']                 = 'トピック';
+$lang['toolbar_icon']          = 'タグ構文の挿入';
+$lang['missing_pagelistplugin'] = 'トピックリストのためにページリストプラグインをインストールしてください。';

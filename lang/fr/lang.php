bf75fa3b399c414de2efe9255b132d4b3d5c6996
NicolasFriedli <nicolas@theologique.ch>
2013-12-07 10:30:49 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,16 +1,16 @@
 <?php
+
 /**
- * French language file
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author NicolasFriedli <nicolas@theologique.ch>
  */
-
-// custom language strings for the plugin
-$lang['tags']  = 'Tags';
-$lang['topic'] = 'Topic';
-$lang['toolbar_icon'] = 'Insérer des Tags';
-$lang['empty_output'] = 'Pas de résultat';
-
+$lang['tags']                  = 'Tags';
+$lang['topic']                 = 'Topic';
+$lang['toolbar_icon']          = 'Insérer des Tags';
+$lang['empty_output']          = 'Pas de résultat';
 $lang['missing_pagelistplugin'] = 'Le plugin Pagelist doit être installé pour la syntaxe topic.';
-
-//Setup VIM: ex: et ts=2 :
+$lang['include']               = 'Inclure';
+$lang['exclude']               = 'Exclure';
+$lang['use_and']               = 'Utiliser AND';
+$lang['no_tags']               = 'Aucun tag trouvé';

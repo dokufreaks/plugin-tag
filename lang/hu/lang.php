d5146b6f2d9a320dc415c99e308224832fab948c
Marina Vladi <deldadam@gmail.com>
2014-01-22 23:40:20 +0100

    translation update

---
+++ lang/hu/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Marina Vladi <deldadam@gmail.com>
+ */
+$lang['tags']                  = 'Címkék';
+$lang['topic']                 = 'Téma';
+$lang['toolbar_icon']          = 'Címkeszintaxis beillesztése';
+$lang['empty_output']          = 'Üres kimenet';
+$lang['missing_pagelistplugin'] = 'A pagelist bővítményt telepíteni kell a témák listájához.';
+$lang['include']               = 'Beleértve';
+$lang['exclude']               = 'Kivéve';
+$lang['use_and']               = 'AND használata';
+$lang['no_tags']               = 'Nincsenek címkék.';

e8b7ee5d1fd7e49da497a0105b042159d4464837
hib <hibratt@gmail.com>
2014-01-29 11:05:16 +0100

    translation update

---
+++ lang/sv/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author hib <hibratt@gmail.com>
+ */
+$lang['tags']                  = 'Etiketter';
+$lang['topic']                 = 'Ämne';
+$lang['toolbar_icon']          = 'Skriv etikettsyntax';
+$lang['empty_output']          = 'Tom utdata';
+$lang['missing_pagelistplugin'] = 'Pagelist Plugin måste vara installerad vid ämneslista';
+$lang['include']               = 'Inkludera';
+$lang['exclude']               = 'Uteslut';
+$lang['use_and']               = 'Använd AND';
+$lang['no_tags']               = 'Det finns inga etiketter';

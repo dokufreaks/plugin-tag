005b9c3ce5b42164b14d11c2b39cf066c4ad7a86
Hideaki SAWADA <chuno@live.jp>
2014-02-22 12:52:06 +0100

    translation update

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -3,12 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
- * @author     Ikuo Obataya <cxx05051@nifty.com>
+ * @author Ikuo Obataya <cxx05051@nifty.com>
  * @author Hideaki SAWADA <chuno@live.jp>
  */
 $lang['namespace']             = 'タグのデフォルト名前空間';
 $lang['toolbar_icon']          = 'ツールバーボタンを表示（表示されない場合、data/cache/* とブラウザキャッシュを削除）';
-$lang['sortkey']               = 'トピックリストのソート基準';
+$lang['sortkey']               = 'topic 一覧のソート基準';
 $lang['sortkey_o_cdate']       = '作成日';
 $lang['sortkey_o_mdate']       = '更新日';
 $lang['sortkey_o_pagename']    = 'ページ名';
@@ -18,5 +18,5 @@ $lang['sortkey_o_title']       = 'タイトル';
 $lang['sortorder']             = 'ソート順';
 $lang['sortorder_o_ascending'] = '昇順';
 $lang['sortorder_o_descending'] = '降順';
-$lang['pagelist_flags']        = 'タグリストの表示フラグ（カンマ区切り。使用可能フラグはページリストプラグインを参照）';
-$lang['list_tags_of_subns']    = '指定名前空間の下位の名前空間のタグも一覧に含む (count syntax)';
+$lang['pagelist_flags']        = 'タグリストの表示フラグ（カンマ区切り。使用可能フラグは pagelist プラグインを参照）';
+$lang['list_tags_of_subns']    = '指定名前空間の下位の名前空間のタグも一覧に含む（count 構文）';

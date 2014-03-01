35677ecdcaee4c82ec70ded9a4b30733de74ea84
Aleksandr Selivanov <alexgearbox@yandex.ru>
2014-03-01 15:20:36 +0100

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -4,6 +4,11 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['tags']                  = 'Теги';
 $lang['topic']                 = 'Темы';
+$lang['include']               = 'Включать';
+$lang['exclude']               = 'Исключать';
+$lang['use_and']               = 'Использовать логическое И';
+$lang['no_tags']               = 'Теги не найдены';

c77de1242e4e9636c63d04d98c9ed496034b3d9c
Sebastian Klemm <sebastian_klemm@freenet.de>
2014-04-18 23:40:27 +0200

    translation update

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Sebastian Klemm <sebastian_klemm@freenet.de>
+ */
+$lang['tags']                  = 'Tags';
+$lang['topic']                 = 'Thema';
+$lang['toolbar_icon']          = 'Tag-Syntax einfügen';
+$lang['empty_output']          = 'Leere Ausgabe';
+$lang['include']               = 'Einschließen';
+$lang['exclude']               = 'Ausschließen';
+$lang['use_and']               = 'Benutze "UND"';
+$lang['no_tags']               = 'Keine Tags gefunden';

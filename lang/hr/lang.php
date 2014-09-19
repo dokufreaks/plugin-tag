cc2d9ab2ecce1356cbb13b6bd99caaec77bdfbe4
Davor Turkalj <turki.bsc@gmail.com>
2014-09-19 13:20:05 +0200

    translation update

---
+++ lang/hr/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Davor Turkalj <turki.bsc@gmail.com>
+ */
+$lang['tags']                  = 'Oznake';
+$lang['topic']                 = 'Teme';
+$lang['toolbar_icon']          = 'Ubaci sintaksu oznake';
+$lang['empty_output']          = 'Prazan izlaz';
+$lang['include']               = 'Uključi';
+$lang['exclude']               = 'Izuzmi';
+$lang['use_and']               = 'Koristi AND';
+$lang['no_tags']               = 'Nema oznaka';

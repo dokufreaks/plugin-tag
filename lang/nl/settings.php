8f06d3d083fc059b5fe26924be4ab72f3b530aa8
Peter van Diest <peter.van.diest@xs4all.nl>
2015-07-09 08:45:05 +0200

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ * @author Peter van Diest <peter.van.diest@xs4all.nl>
  */
 $lang['namespace']             = 'Standaard namespace voor tags';
 $lang['toolbar_icon']          = 'Geef een werkbalkknop weer in het bewerkvenster';
@@ -19,3 +20,6 @@ $lang['sortorder_o_ascending'] = 'Oplopend';
 $lang['sortorder_o_descending'] = 'Aflopend';
 $lang['pagelist_flags']        = 'Opmaakvlaggen voor de taglijst (komma gescheiden, voor beschikbare vlaggen zie documentatie van de Pagelist plugin)';
 $lang['list_tags_of_subns']    = 'Als er op een specifieke namespace wordt gezocht, geef dan in de lijst ook tags uit subnamespaces van deze specifieke namespace (count syntax)';
+$lang['tags_list_css']         = 'Kies een style voor de lijst van tags voor de pagina';
+$lang['tags_list_css_o_tags']  = 'Standaard style';
+$lang['tags_list_css_o_tagstop'] = 'Geptimaliseerd voor een lijst van tags bovenaan de pagina';

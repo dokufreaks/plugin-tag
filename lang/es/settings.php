aba8cc3cdb88b58d36856135811733d9efdb058a
Domingo Redal <docxml@gmail.com>
2015-08-01 12:35:29 +0200

    translation update

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -5,6 +5,7 @@
  * 
  * @author Digna González Otero <digna.gonzalezotero [at] gmail [dot] com>
  * @author Camilo Sampedro <sampedro1903@gmail.com>
+ * @author Domingo Redal <docxml@gmail.com>
  */
 $lang['namespace']             = 'Espacio de nombres por defecto para las etiquetas';
 $lang['toolbar_icon']          = 'Mostrar el ícono en la barra (borra data/cache/ y el caché del navegador si no es visible)';
@@ -20,3 +21,6 @@ $lang['sortorder_o_ascending'] = 'ascendente';
 $lang['sortorder_o_descending'] = 'descendente';
 $lang['pagelist_flags']        = 'Dándole formato para la lista de etiquetas (separadas por coma, para ver los banderas (flags) consultar la documentación del plugin)';
 $lang['list_tags_of_subns']    = 'Listar también etiquetas en subespacios de nombre en un espacio de nombre específico (recuento de sintaxis)';
+$lang['tags_list_css']         = 'Elija el estilo aplicado a la página de lista de etiquetas';
+$lang['tags_list_css_o_tags']  = 'estilo por defecto';
+$lang['tags_list_css_o_tagstop'] = 'optimizado para la lista de etiquetas en la parte superior de la página';

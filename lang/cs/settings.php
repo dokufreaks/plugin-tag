818acefdec92b263a5fa45a81cdfafbfc9c0262f
kochi <kochi@centrum.cz>
2015-09-20 20:19:42 +0200

    czech trenslation

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -7,16 +7,27 @@
  */
  
 // for the configuration manager
-$lang['namespace']      = 'Implicitní jmenný prostor pro tagy';
+$lang['namespace']      = 'Výchozí jmenný prostor pro tagy';
+$lang['toolbar_icon']   = 'Zobrazit toolbar ikonku (pokud se nezobrazuje, smažte data/cache/* a cache prohlížeče)';
 
 $lang['sortkey']               = 'razení zápisků podle';
 $lang['sortkey_o_cdate']       = 'data vytvoření';
 $lang['sortkey_o_mdate']       = 'data modifikace';
 $lang['sortkey_o_pagename']    = 'názvu stránky';
 $lang['sortkey_o_id']          = 'ID stránky';
+$lang['sortkey_o_ns']           = 'jmenný prostor';
+$lang['sortkey_o_title']        = 'nadpis';
 
 $lang['sortorder']              = 'pořadí';
 $lang['sortorder_o_ascending']  = 'vzestupné';
 $lang['sortorder_o_descending'] = 'sestupné';
+
+$lang['pagelist_flags'] = 'Formátovací flags pro list tagů (comma-separated, viz dostupné flags v dokumentaci pluginu pagelist)';
+
+$lang['list_tags_of_subns'] = 'List také tags in podjmenných prostorů daného jmenného prostoru';
+
+$lang['tags_list_css']           = 'Vyber styl aplikovaný na stránku seznam tagů  ';
+$lang['tags_list_css_o_tags']    = 'výchozí styl';
+$lang['tags_list_css_o_tagstop'] = 'optimalizováno pro zobrazení seznamu tagů v horní části stránky';
 
 //Setup VIM: ex: et ts=2 enc=utf-8 :

e88a6c7e2bc30f76c08cf30e580b6998f01267c3
Alan Davies <ben.brynsadler@gmail.com>
2015-12-31 14:25:06 +0100

    translation update

---
+++ lang/cy/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Alan Davies <ben.brynsadler@gmail.com>
+ */
+$lang['tags']                  = 'Tagiau';
+$lang['topic']                 = 'Testun';
+$lang['toolbar_icon']          = 'Mewnosod Cystrawen-Tag';
+$lang['empty_output']          = 'Allbwn Gwag';
+$lang['include']               = 'Cynnwys';
+$lang['exclude']               = 'Eithrio';
+$lang['use_and']               = 'Defnyddio AC';
+$lang['no_tags']               = 'Dim tagiau wedi\'u darganfod';

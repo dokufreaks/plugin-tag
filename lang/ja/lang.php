4f74a26a9d6007578554f90b8966ba1921639915
Hideaki SAWADA <chuno@line.jp>
2016-01-18 17:40:08 +0100

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -5,10 +5,13 @@
  * 
  * @author Ikuo Obataya <cxx05051@nifty.com>
  * @author Hideaki SAWADA <chuno@live.jp>
+ * @author Hideaki SAWADA <chuno@line.jp>
  */
+$lang['tag']                   = 'タグ';
 $lang['tags']                  = 'タグ';
 $lang['topic']                 = 'トピック';
-$lang['toolbar_icon']          = 'tag 構文の挿入';
+$lang['count']                 = '#';
+$lang['toolbar_icon']          = 'タグ構文の挿入';
 $lang['empty_output']          = '空の出力';
 $lang['include']               = '含む';
 $lang['exclude']               = '除外する';

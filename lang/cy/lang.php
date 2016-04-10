a7b34fef70fb051fc710aad3bf6e714d49a021cf
diafol <ben.brynsadler@gmail.com>
2016-04-10 18:45:29 +0200

    translation update

--- lang/cy/lang.php
+++ lang/cy/lang.php
@@ -4,9 +4,12 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Alan Davies <ben.brynsadler@gmail.com>
+ * @author diafol <ben.brynsadler@gmail.com>
  */
+$lang['tag']                   = 'Tag';
 $lang['tags']                  = 'Tagiau';
 $lang['topic']                 = 'Testun';
+$lang['count']                 = '#';
 $lang['toolbar_icon']          = 'Mewnosod Cystrawen-Tag';
 $lang['empty_output']          = 'Allbwn Gwag';
 $lang['include']               = 'Cynnwys';

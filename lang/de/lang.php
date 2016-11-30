fff74bf5242e2ebfceb12c982f4692ac2ed2ffcf
rnck <dokuwiki@rnck.de>
2016-11-30 08:15:10 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Esther Brunner <wikidesign@gmail.com>
  * @author Dominik Eckelmann <deckelmann@gmail.com>
  * @author Dennis Plöger <develop@dieploegers.de>

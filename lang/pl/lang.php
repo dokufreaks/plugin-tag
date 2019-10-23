a8eba19bdbf4ca2ce3dc9fc744c43bf5f94e0dcc
Bartek S <sadupl@gmail.com>
2019-10-23 06:55:07 +0200

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['tag']                   = 'Tag';
+$lang['tags']                  = 'Tagi';
+$lang['topic']                 = 'Temat';
+$lang['count']                 = '#';
+$lang['include']               = 'Uwzględnić';
+$lang['exclude']               = 'Wykluczyć';
+$lang['no_tags']               = 'Nie znaleziono tagów';

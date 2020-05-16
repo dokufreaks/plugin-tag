336061991091a9c16f7d1eb4348cc717e8d46597
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2020-05-16 13:00:21 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,17 +2,17 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
  * @author NicolasFriedli <nicolas@theologique.ch>
- * @author Nicolas Friedli <nicolas@theologique.ch>
  */
-$lang['tag']                   = 'Tag';
-$lang['tags']                  = 'Tags';
-$lang['topic']                 = 'Topic';
+$lang['tag']                   = 'Étiquette';
+$lang['tags']                  = 'Étiquettes';
+$lang['topic']                 = 'Sujet';
 $lang['count']                 = '#';
-$lang['toolbar_icon']          = 'Insérer des Tags';
+$lang['toolbar_icon']          = 'Insérer des étiquettes';
 $lang['empty_output']          = 'Pas de résultat';
 $lang['include']               = 'Inclure';
 $lang['exclude']               = 'Exclure';
 $lang['use_and']               = 'Utiliser AND';
-$lang['no_tags']               = 'Aucun tag trouvé';
+$lang['no_tags']               = 'Aucune étiquette trouvée';

b3728c47f0b4e90e65a998c2314d0dfacc9a9e75
clso <694743+clso@users.noreply.github.com>
2021-08-16 05:04:31 +0800

    Update metadata.php

--- conf/metadata.php
+++ conf/metadata.php
@@ -16,5 +16,6 @@ $meta['toolbar_icon']       = array('onoff');
 $meta['list_tags_of_subns'] = array('onoff');
 $meta['tags_list_css']      = array('multichoice',
                                     '_choices' => array('tags', 'tagstop'));
+$meta['tags_exclude'] = array('string');
 
 //Setup VIM: ex: et ts=2 :

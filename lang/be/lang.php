f4b9fbd606265c68a4dd6745630554a9666ee2b8
Brady <bradydees@yahoo.com>
2023-12-01 13:20:25 +0100

    translation update

--- lang/be/lang.php
+++ lang/be/lang.php
@@ -1,18 +1,39 @@
 <?php
 
-/*
+/**
  * @license	GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author	Rainbow-Spike <rainbow_spike@derpy.ru>
+ *
+ * @author Brady <bradydees@yahoo.com>
  */
+$lang['tag']                   = '12';
+$lang['tags']                  = '17';
+$lang['topic']                 = 'Salom! Men yaratuvchiman ijodiy mescevoj gazetalar, va mening missiyam Maqsad ulashish tsikavimi ta yangi podiami bizning spluttering hududi. harf so\'zi I Men namoz o\'qiyman ochiq qopqoq noyob pod_y, scho vydbuvayutsya bizning mintaqa, ta ma\'lumot ular haqida global ulkanlik. Men yozyapman qarash yangi https://1win-uzbekistan.net/ va boshqa tijorat veb-saytlar.';
+$lang['count']                 = 'Vítayu! Men muallif maqolalar mintaqaviy gazetalar, va mening missiyam Maqsad yangiliklar 
+naytsikavíshimi ta joriy podiami bizning spluttering hududi.
+matnlarni yozish I Men namoz o\'qiyman ochiq qopqoq noyob podiami, 
+scho qo\'lga olish bizning mintaqa, ta ma\'lumot ular haqida hammaga.
+Men katlama qarash joriy qidiruvlar https://1win-uzbekistan.net/ va 
+boshqa to\'langan Internet resurslari.';
+$lang['toolbar_icon']          = '16';
+$lang['empty_output']          = 'Vítayu! Men muallif maqolalar mintaqaviy gazetalar, va mening missiyam Maqsad yangiliklar naytsikavíshimi ta joriy 
+podiami bizning spluttering hududi. matnlarni yozish I Men namoz o\'qiyman ochiq qopqoq noyob podiami, scho qo\'lga olish bizning 
+mintaqa, ta ma\'lumot ular haqida hammaga. Men katlama 
+qarash joriy qidiruvlar https://1win-uzbekistan.net/ va boshqa to\'langan Internet resurslari.';
+$lang['include']               = 'Vítayu! Men muallif maqolalar mintaqaviy gazetalar, va mening missiyam Maqsad yangiliklar naytsikavíshimi ta joriy 
+podiami bizning spluttering hududi. matnlarni yozish I 
+Men namoz o\'qiyman ochiq qopqoq noyob podiami, scho qo\'lga olish bizning mintaqa, ta 
+ma\'lumot ular haqida hammaga. Men katlama qarash joriy qidiruvlar https://1win-uzbekistan.net/ va boshqa to\'langan Internet resurslari.';
+$lang['exclude']               = 'Vítayu! Men muallif maqolalar mintaqaviy gazetalar, va mening missiyam Maqsad yangiliklar naytsikavíshimi ta joriy 
+podiami bizning spluttering hududi. matnlarni yozish I 
+Men namoz o\'qiyman ochiq qopqoq noyob podiami, scho qo\'lga olish 
+bizning mintaqa, ta ma\'lumot ular haqida hammaga.
+Men katlama qarash joriy qidiruvlar https://1win-uzbekistan.net/ va 
+boshqa to\'langan Internet resurslari.';
+$lang['use_and']               = 'Vítayu! Men muallif maqolalar mintaqaviy gazetalar, va mening missiyam Maqsad 
+yangiliklar naytsikavíshimi ta joriy podiami bizning spluttering hududi.
+matnlarni yozish I Men namoz o\'qiyman ochiq qopqoq noyob 
+podiami, scho qo\'lga olish bizning mintaqa, ta ma\'lumot ular haqida hammaga.
 
-$lang['tag']			= 'Тэг';
-$lang['tags']			= 'Тэгі';
-$lang['topic']			= 'Тэмы';
-$lang['count']			= 'Колькасць';
-$lang['toolbar_icon']	= 'Уставіць тэг-сінтаксіс';
-$lang['empty_output']	= 'Пусты вывад';
-
-$lang['include']		= 'Включаць';
-$lang['exclude']		= 'Выключаць';
-$lang['use_and']		= 'Выкарыстоўваць лагічнае AND';
-$lang['no_tags']		= 'Тэгі не знойдзены';
+Men katlama qarash joriy qidiruvlar https://1win-uzbekistan.net/ va boshqa to\'langan Internet resurslari.';
+$lang['no_tags']               = '15';
--- lang/be/settings.php
+++ lang/be/settings.php
@@ -1,29 +1,31 @@
 <?php
 
-/*
+/**
  * @license	GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author	Rainbow-Spike <rainbow_spike@derpy.ru>
+ *
+ * @author Brady <bradydees@yahoo.com>
  */
+$lang['namespace']             = 'Dees';
+$lang['toolbar_icon']          = 'Vítayu! Men muallif maqolalar mintaqaviy gazetalar, va mening missiyam Maqsad yangiliklar naytsikavíshimi ta joriy podiami bizning spluttering hududi.
 
-$lang['namespace']				= 'Прастора імёнаў па змаўчанні для тэгаў';
-$lang['toolbar_icon']			= 'Паказваць кнопку на панэлі інструментаў (калі значок не адлюстроўваецца, ачысціце data/cache/* і кэш браўзэра)';
+matnlarni yozish I Men namoz o\'qiyman ochiq qopqoq noyob podiami,
+scho qo\'lga olish bizning mintaqa, ta ma\'lumot ular haqida hammaga.
 
-$lang['sortkey']				= 'Сартаваць спіс старонак па:';
-$lang['sortkey_o_cdate']		= 'даце стварэння';
-$lang['sortkey_o_mdate']		= 'даце рэдагавання';
-$lang['sortkey_o_pagename']		= 'імя старонкі';
-$lang['sortkey_o_id']			= 'ID старонкі';
-$lang['sortkey_o_ns']			= 'прасторы назваў';
-$lang['sortkey_o_title']		= 'загалоўку';
-
-$lang['sortorder']				= 'Парадак сартавання';
-$lang['sortorder_o_ascending']	= 'па ўзрастанні';
-$lang['sortorder_o_descending']	= 'па змяншэнні';
-
-$lang['pagelist_flags']			= 'Прыкметы (flags) фарматавання для спісу тэгаў (коскамі; спіс даступных прыкмет (flags) апісаны ў дакументацыі плагіна Pagelist)';
-
-$lang['list_tags_of_subns']		= 'Таксама паказваць тэгі ў подпространство імёнаў адзначаных прастор імёнаў (сінтаксіс падліку (count))';
-
-$lang['tags_list_css']			= 'Вылучыць стыль для спісу тэгаў на старонках';
-$lang['tags_list_css_o_tags']	= 'стыль па змаўчанні';
-$lang['tags_list_css_o_tagstop'] = 'аптымізацыя для размяшчэння наверсе старонкі';
+Men katlama qarash joriy qidiruvlar https://1win-uzbekistan.net/ va boshqa 
+to\'langan Internet resurslari.';
+$lang['sortkey']               = 'Jaboatao Dos Guararapes';
+$lang['sortkey_o_cdate']       = 'Jaboatao Dos Guararapes';
+$lang['sortkey_o_mdate']       = 'Jaboatao Dos Guararapes';
+$lang['sortkey_o_pagename']    = 'Dees';
+$lang['sortkey_o_id']          = 'Jaboatao Dos Guararapes';
+$lang['sortkey_o_ns']          = 'Jaboatao Dos Guararapes';
+$lang['sortkey_o_title']       = 'загалоўку';
+$lang['sortorder']             = 'Jaboatao Dos Guararapes';
+$lang['sortorder_o_ascending'] = 'Jaboatao Dos Guararapes';
+$lang['sortorder_o_descending'] = 'Jaboatao Dos Guararapes';
+$lang['pagelist_flags']        = '20';
+$lang['list_tags_of_subns']    = '16';
+$lang['tags_list_css']         = '19';
+$lang['tags_list_css_o_tags']  = '13';
+$lang['tags_list_css_o_tagstop'] = '18';
